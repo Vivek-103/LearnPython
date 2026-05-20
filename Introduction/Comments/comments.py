dc68d94962de84a55b2adfeee5696960bd8fa7fd
Vivek-103 <viveksrivastava1003@gmail.com>
2026-05-20 16:44:00 +0530

    Intro to Python

---
+++ Introduction/Comments/comments.py
@@ -0,0 +1,10 @@
+# Comments are Something which are ignored by the interpretor
+# Multi line comments are not available in python but we can Achieve it using doc string
+
+"""hello this is multiline
+
+whats up FAAAHHHHHHHH!!!!
+
+"""
+
+print("Learning Comments in Python")
