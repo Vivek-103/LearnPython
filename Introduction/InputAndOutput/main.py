03682ea9f7bd9f89b01a520a7bc114a533a894ab
Vivek-103 <viveksrivastava1003@gmail.com>
2026-05-21 18:09:15 +0530

    Introduction Part Completed

---
+++ Introduction/InputAndOutput/main.py
@@ -0,0 +1,24 @@
+# OUTPUT :
+
+# To Provide output of the code we have written we have to use print() function
+
+# EX : name = input("Enter your name") , print(f"Hello , {name}!")
+
+####################################################################################################################################
+
+# INPUT :
+
+# To ask user some info we can use input()
+
+# Default fata type of input is always string , you have to manually convert the data type of input statements
+
+# ASSIGNMENTS
+# 1. Accept Numbers from a User
+
+number = int(input("Enter the number "))
+print("You entered" , number)
+print(type(number))
+# 2. Accept Age from user and print it.
+age = input ("Enter your age ")
+print("your age is" , age) 
+print(type(age))
